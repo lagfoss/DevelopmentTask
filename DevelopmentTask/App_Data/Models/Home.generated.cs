7771b767c056068f86faf813fcc6be11dd423d4c
lagfoss <32448248+lagfoss@users.noreply.github.com>
2020-04-08 23:47:28 +0200

    Produkter på forsiden, kunne ikke få dem til at være dynamiske med list view, så nu er de statiske på forsiden

--- DevelopmentTask/App_Data/Models/Home.generated.cs
+++ DevelopmentTask/App_Data/Models/Home.generated.cs
@@ -45,6 +45,90 @@ namespace Umbraco.Web.PublishedModels
 		// properties
 
 		///<summary>
+		/// Product1Items
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder", "8.6.0")]
+		[ImplementPropertyType("product1Items")]
+		public global::System.Web.IHtmlString Product1Items => this.Value<global::System.Web.IHtmlString>("product1Items");
+
+		///<summary>
+		/// Product1Name
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder", "8.6.0")]
+		[ImplementPropertyType("product1Name")]
+		public string Product1Name => this.Value<string>("product1Name");
+
+		///<summary>
+		/// Product1Price
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder", "8.6.0")]
+		[ImplementPropertyType("product1Price")]
+		public string Product1Price => this.Value<string>("product1Price");
+
+		///<summary>
+		/// Product2Items
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder", "8.6.0")]
+		[ImplementPropertyType("product2Items")]
+		public global::System.Web.IHtmlString Product2Items => this.Value<global::System.Web.IHtmlString>("product2Items");
+
+		///<summary>
+		/// Product2Name
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder", "8.6.0")]
+		[ImplementPropertyType("product2Name")]
+		public string Product2Name => this.Value<string>("product2Name");
+
+		///<summary>
+		/// Product2Price
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder", "8.6.0")]
+		[ImplementPropertyType("product2Price")]
+		public string Product2Price => this.Value<string>("product2Price");
+
+		///<summary>
+		/// Product3Items
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder", "8.6.0")]
+		[ImplementPropertyType("product3Items")]
+		public global::System.Web.IHtmlString Product3Items => this.Value<global::System.Web.IHtmlString>("product3Items");
+
+		///<summary>
+		/// Product3Name
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder", "8.6.0")]
+		[ImplementPropertyType("product3Name")]
+		public string Product3Name => this.Value<string>("product3Name");
+
+		///<summary>
+		/// Product3Price
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder", "8.6.0")]
+		[ImplementPropertyType("product3Price")]
+		public string Product3Price => this.Value<string>("product3Price");
+
+		///<summary>
+		/// Product4Items
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder", "8.6.0")]
+		[ImplementPropertyType("product4Items")]
+		public global::System.Web.IHtmlString Product4Items => this.Value<global::System.Web.IHtmlString>("product4Items");
+
+		///<summary>
+		/// Product4Name
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder", "8.6.0")]
+		[ImplementPropertyType("product4Name")]
+		public string Product4Name => this.Value<string>("product4Name");
+
+		///<summary>
+		/// Product4Price
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder", "8.6.0")]
+		[ImplementPropertyType("product4Price")]
+		public string Product4Price => this.Value<string>("product4Price");
+
+		///<summary>
 		/// ProductsHeadline
 		///</summary>
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder", "8.6.0")]
